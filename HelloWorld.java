9dae71d7bf58e1ab9cb4b4e0f0c2f5986efb97c9
ChadCarey <car10038@byui.edu>
2014-05-13 14:13:09 -0600

    This is a simple HelloWorld.java program

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+﻿public class HelloWorld {
+
+	public class sayHello() {
+		System.out.println("HELLO GIT HUB!");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld h = new HelloWorld;
+		h.sayHello();
+	}
+}

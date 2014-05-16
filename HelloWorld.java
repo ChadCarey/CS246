c6a95a872b210ccf8c2d61f801977f0855323bc9
ChadCarey7 <chadcarey7@gmail.com>
2014-05-16 11:40:54 -0600

    small change to text in hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public void sayHello() {
-	System.out.println("Hello World");
+	System.out.println("Hello GitGub World");
     }
 
     public static void main(String[] args) {

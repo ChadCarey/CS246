4a73138bd9e3c3da357e324477b2447b9440892f
ChadCarey7 <chadcarey7@gmail.com>
2014-05-16 12:45:41 -0600

    pusing to test for pracice

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public void sayHello() {
 	System.out.println("Hello GitGub World");
-
+	System.out.println("Test");
     }
 
     public static void main(String[] args) {
